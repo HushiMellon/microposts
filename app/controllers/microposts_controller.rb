57ae731e99db68bf174a7592bdcfe4207a7a7185
hushimellon <hushimellon@gmail.com>
2015-08-12 14:30:36 +0000

    kaminariOK

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -19,6 +19,9 @@ class MicropostsController < ApplicationController
     redirect_to request.referrer || root_url
   end
 
+ # def feed
+  #    @feed = Micropost.page(params[:page])
+  #end
   
   private
   def micropost_params
